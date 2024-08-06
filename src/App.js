77fb1846048aadf3a506a7a853e689abddc2ab27
Tharindu Bandara <tharindu.bandara@madmobile.com>
2024-08-06 15:34:34 +0530

    initial demo

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+
+function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch('/api/hello')
+      .then(response => response.text())
+      .then(data => setMessage(data))
+      .catch(error => console.error('Error:', error));
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>{message ? message : 'Loading...'}</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
